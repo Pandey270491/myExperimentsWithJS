83063d5d651d552a5e4c3c768896abf5443c96ca
Pandey270491 <shraddhapandeysh@gmail.com>
2017-06-27 17:54:25 +0530

    Data to Practice for JS Arrays

---
+++ JS_Fundamentals_Array_day4/Array_Cardio_Day4.js
@@ -0,0 +1,18 @@
+//Array Cardio Day #1
+
+const inventors=[
+	{ first: 'Albert', last:'Einstein', year:1879, passed: 1955, contribution: 'Thoery of Relativity' },
+	{ first: 'Issac', last:'Newton', year:1643, passed: 1727 , contribution: 'Mechanics and gravitation'},
+	{ first: 'Galeleo', last:'Galilei', year: 1564, passed: 1642 , contribution: 'Observational Astronomy'},
+	{ first: 'Marie', last:'Curie', year:1867, passed: 1934 , contribution: 'Observational Astronomy'},
+	{ first: 'Johhanes', last:'Keppler', year:1571, passed: 1630 , contribution: ' laws of planetary motion' },
+	{ first: 'Nicolas', last:'Copernicus', year:1473, passed: 1543 , contribution: 'Model of the Universe - Sun is the center'},
+	{ first: 'Max', last:'Plank', year: 1858, passed: 1947 , contribution: 'Originator of Quantum Theory'},
+];
+
+const people=['Beck,Glenn','Becker,Carl','Beckett,Samuel','Beddoes,Mick','Beecher,Henry','Beethoven,Ludwig','Begin,Menachem',
+			  'Belloc,Helaire','Bello,Saul','Benchley,Robert','Benenson,Peter','Ben-Gurion-David','Benjamin,Walter','Benn,Tony',
+			  'Bennington,Chester','Benson,Leana','Bent,Silas','Bentsen,Lloyd','Berger,Ric','Bergman,Ingmar','Berio,Luciano',
+			  'Berle,Milton','Berlin,Irving','Berle,Eric','Bernhard,Sandra','Berra,Yogi','Berry,Halle','Berry,Wendell','Bethea,Erin'
+			  'Beven,Aneurin','Bevel,Ken','Biden,Joseph','Bierce,Ambrose','Biko,Steve','Billings,Josh','Biondo,Frank','Birelle,Augustine',
+			  'Black,Elk','Blair,Robert','Blair,Tony','Blake,William'];
